44855dcf0f91d58ed422a16f2663941ef3b29ebd
Amy KWong <amykwong6@gmail.com>
2023-08-12 10:31:00 -0400

    comimiting to git hub

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import accountReducer from './redux/accountSlice';
+import App from './App';
+
+// Create the Redux store
+const store = configureStore({
+   // ***TODO***
+  
+   // Set the accountReducer as the reducer for the 'account' state
+  reducer: {
+    account: accountReducer
+  }
+
+
+});
+
+// Render the App component inside the Provider component
+ReactDOM.createRoot(document.getElementById("root")).render(
+
+  <Provider store= {store}>
+    <App />
+  </Provider>
+   // ***TODO***
+  
+);
